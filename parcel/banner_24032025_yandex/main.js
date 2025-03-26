1e3e1c34abf0d9a4111151e973efb2cc0e517950
aigri <aigre87@gmail.com>
2025-03-26 19:52:40 +0300

    add new banners work in progress

--- parcel/banner_24032025_yandex/main.js
+++ parcel/banner_24032025_yandex/main.js
@@ -6,17 +6,26 @@ function getRandomSubset(arr, part) {
     const subsetSize = Math.ceil(arr.length / 3); // Get 1/3
     return shuffled.slice(0, subsetSize);
 }
+function getRandomInclusive(n) {
+    return Math.floor(Math.random() * (n + 1));
+}
 function getMatchingClass(element) {
     return Object.keys(squareSizes).find(size => element.classList.contains(size)) || null;
 }
 const squareSizeDefault = 20;
 const squareSizes = {
     's728x90':  20,
+    's320x50':  10,
+    's320x100':  14,
+    's970x250':  30,
+    's480x320':  40,
 };
 const banners = document.querySelectorAll(".banner");
 banners.forEach((banner, i) => {
     const container = banner.querySelector(".animation");
+    let activeElsTimelines = [];
     const matchingClass = banner ? getMatchingClass(banner) : null;
+    let clickTl;
     let squareSize = squareSizeDefault;
     if(matchingClass){
         squareSize = squareSizes[matchingClass];
@@ -52,32 +61,52 @@ banners.forEach((banner, i) => {
     //     })
     // }
     function action(el) {
-        const dur = gsap.utils.random(2, 3, 0.2); // 0.5-2
-        const delay = gsap.utils.random(0, 24, 0.2); // 5-10
+        // Если элемент уже анимируется, не запускаем заново
+        //if (gsap.getTweensOf(el).length > 0) return;
+
+        const dur = gsap.utils.random(2, 3, 0.2);
+        const delay = gsap.utils.random(0, 40, 0.2);
 
         const elTl = gsap.timeline({
+            id: `action-${el.dataset.idx}`,
             paused: false,
             delay: delay,
             onComplete: () => {
-                action(el)
+                //console.log('onComplete action');
+                if (!clickTl || !clickTl.isActive()) {
+                    action(el);
+                }
             }
         });
+
         elTl.to(el, {
             duration: dur,
+            repeatDelay: 1,
             scale: 1,
             yoyo: true,
             repeat: 1,
             ease: "power1.inOut",
-        })
+        });
+
+        activeElsTimelines[el.dataset.idx] = elTl;
     }
 
     function hoverGrid({currentTarget}) {
+        gsap.killTweensOf(currentTarget);
         if(!clickTween || !clickTween.isActive()) {
             gsap.to(currentTarget, {
             duration: 1,
             keyframes: {
-                scale: [1, 0.9, 1],
+                scale: [1.1, 0.9, 1.1],
                 "background-color": ["#c6f05e", "#75a113", "#c6f05e"]
+            },
+            onComplete: () => {
+                const elTweens = gsap.getTweensOf(currentTarget);
+                const isActiveElTween = elTweens.some(tw => tw.isActive())
+                //console.log('elTweens', elTweens, isActiveElTween);
+                gsap.to(currentTarget, { scale: 0, duration: 2.6, ease: "power4.out", onComplete: () => {
+                    action(currentTarget);
+                } });
             },
             ease: "power4.out"
         });
@@ -85,33 +114,42 @@ banners.forEach((banner, i) => {
     }
 
     function animateGrid({currentTarget}) {
-        console.log('repeatTl.isActive()', repeatTl.isActive());
-        if (repeatTl.isActive()) {
-            repeatTl.pause().kill();
+        if (!clickTl || !clickTl.isActive()) {
+            clickTl = gsap.to(container.querySelectorAll(".sqr"), {
+                duration: 2,
+                overwrite: true,
+                keyframes: {
+                    scale: [1, 0.9, 1, 0],
+                    "background-color": ["#c6f05e", "#75a113", "#c6f05e", "#c6f05e"]
+                },
+                ease: "power4.out",
+                onComplete: () => {
+                    //console.log('click complete');
+                    createGrid();
+                    repeatAnimationInit();
+                },
+                stagger: {
+                    onStart: function(){
+                        //console.log(activeElsTimelines[this.targets()[0].dataset.idx]);
+                        activeElsTimelines[this.targets()[0].dataset.idx].kill();
+                    },
+                    grid: grid,
+                    from: Number(currentTarget.dataset.idx),
+                    amount: 1
+                }
+            });
         }
-        clickTl = gsap.to(container.querySelectorAll(".sqr"), {
-            duration: 2,
-            keyframes: {
-                scale: [1, 0.9, 1, 0],
-                "background-color": ["#c6f05e", "#75a113", "#c6f05e", "#c6f05e"]
-            },
-            ease: "power4.out",
-            onComplete: () => {
-                console.log('click complete');
-                action(container);
-            },
-            stagger: {
-                grid: grid,
-                from: Number(currentTarget.dataset.idx),
-                amount: 1
-            }
-        });
     }
 
-    function repeatAnimationInit(){
-        gsap.utils.toArray(container.querySelectorAll(".sqr")).map((el) => {
+    function repeatAnimationInit() {
+        //console.log("repeatAnimationInit started");
+        gsap.killTweensOf(container.querySelectorAll(".sqr"));
+        activeElsTimelines.forEach(tl => tl.kill());
+        activeElsTimelines = [];
+
+        gsap.utils.toArray(container.querySelectorAll(".sqr")).forEach(el => {
             action(el);
-        })
+        });
     }
 
     function updateGrid() {
@@ -119,17 +157,26 @@ banners.forEach((banner, i) => {
         container.style.setProperty("--rows", `${rows}`);
     }
 
-    function createGrid() {
-        //document.body.innerHTML = "";
-
+    function removeGrid() {
+        container.querySelectorAll(".sqr").forEach(sqr => {
+            sqr.removeEventListener("mouseenter", animateGrid);
+        });
         container.innerHTML = ``;
-        for (let i = 0; i < columns * rows; i += 1) {
+    }
+    function createGrid() {
+        removeGrid();
+        const elsLength = columns * rows;
+        const surprize = getRandomInclusive(elsLength);
+        for (let i = 0; i < elsLength; i += 1) {
             const sqr = document.createElement("span");
 
             sqr.classList.add("sqr");
             sqr.dataset.idx = i;
-            // sqr.addEventListener("click", animateGrid);
-            // sqr.addEventListener("mouseover", hoverGrid);
+            if(i === surprize){
+                sqr.addEventListener("mouseenter", animateGrid);
+                sqr.classList.add('special');
+            }
+            //sqr.addEventListener("mouseover", hoverGrid);
 
             container.appendChild(sqr);
         }
