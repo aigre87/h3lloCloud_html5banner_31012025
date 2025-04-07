453173b58b1953ae503bf4f4936ed297cd28e0e1
aigri <aigre87@gmail.com>
2025-04-07 17:58:39 +0300

    add new banners

--- parcel/banner_24032025_vk/main.js
+++ parcel/banner_24032025_vk/main.js
@@ -1,87 +1,47 @@
-//helper function to build a grid of <div> elements
-function buildGrid(vars) {
-	vars = vars || {};
-	var container = document.createElement("div"),
-		rows = vars.grid[0] || 5,
-		cols = vars.grid[1] || 5,
-		width = vars.width || 100,
-		gutter = vars.gutter || 1,
-    className = vars.className || "",
-		w = (width - cols * gutter) / cols,
-		parent = (typeof(vars.parent) === "string") ? document.querySelector(vars.parent) : vars.parent ? vars.parent : document.body,
-		css = "display: inline-block; margin: 0 " + (gutter / width * 100) + "% " + (gutter / width * 100) + "% 0; width: " + (w / width * 100) + "%;",
-		l = rows * cols,
-		i, box;
-	for (i = 0; i < l; i++) {
-		box = document.createElement("div");
-		box.style.cssText = css;
-    box.setAttribute("class", className);
-    box.index = i;
-    box.setAttribute("data-index", i);
-    if (vars.onCellClick) {
-      box.addEventListener("click", vars.onCellClick);
+var target = document.querySelectorAll('.banner');
+
+target.forEach((el, i) => {
+    var mainTl = gsap.timeline({
+        paused: true,
+        yoyo: false,
+        repeat: 0,
+        repeatDelay: 0,
+        immediateRender: false,
+        defaults: {
+            ease: "Power1.easeInOut",
+        }
+    });
+    mainTl.fromTo(el.querySelector('.man'),
+{ autoAlpha: 0, x: -10} ,
+  { autoAlpha: 1, duration: 1.5, delay: 0.2, x: 0 });
+
+
+    var target = el;
+    var alreadyTriggered = false;
+
+    function checkVisibility() {
+        if (alreadyTriggered) return;
+
+        var rect = target.getBoundingClientRect();
+        var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+        // Сколько пикселей элемента видно
+        var visiblePixels = Math.min(rect.bottom, windowHeight) - Math.max(rect.top, 0);
+
+        if (visiblePixels > 0) {
+          var visibleRatio = visiblePixels / rect.height;
+
+          if (visibleRatio >= 0.3) {
+            // 'Более 30% элемента видно (фоллбек для IE10)'
+            alreadyTriggered = true;
+            mainTl.play();
+            window.removeEventListener('scroll', checkVisibility);
+            window.removeEventListener('resize', checkVisibility);
+          }
+        }
     }
-		container.appendChild(box);
-	}
-	container.style.cssText = "width:" + width + "px; line-height: 0; padding:" + gutter + "px 0 0 " + gutter + "px; display:inline-block;";
-	parent.appendChild(container);
-	return container;
-}
 
-var grid = [5,13], //[rows, columns]
-    tl = gsap.timeline({repeat: -1, repeatDelay: 0.5});
-buildGrid({grid: grid, className: "box", width: 1000, gutter: 15, parent: ".animation"});
-tl.to(".box", {
-      duration: 1,
-      scale: 0.1,
-      y: 60,
-      yoyo: true,
-      repeat: 1,
-      ease: "power1.inOut",
-      stagger: {
-        amount: 1.5,
-        grid: grid,
-        axis: axis,
-        ease: ease,
-        from: "random"
-      }
-    }
-  );
-
-//this just helps avoid the pixel-snapping that some browsers do.
-gsap.set(".box", {rotation: 0.5, force3D: true});
-
-// // DEBUG
-// function createTimelineControls(timeline, containerId) {
-//     const controls = document.createElement("div");
-//     controls.innerHTML = `
-//     <div class="ttl"></div>
-//     <button class="play">Play</button>
-//     <button class="pause">Pause</button>
-//     <button class="restart">Restart</button>
-//     <input type="range" class="slider" min="0" max="1" step="0.01" value="0">
-//   `;
-//     document.body.appendChild(controls);
-//     controls.style.position = 'relative';
-//     controls.style.zIndex = 1000;
-//
-//     const ttl = controls.querySelector(".ttl");
-//     const playButton = controls.querySelector(".play");
-//     const pauseButton = controls.querySelector(".pause");
-//     const restartButton = controls.querySelector(".restart");
-//     const slider = controls.querySelector(".slider");
-//     ttl.innerHTML = containerId;
-//
-//     playButton.addEventListener("click", () => timeline.play());
-//     pauseButton.addEventListener("click", () => timeline.pause());
-//     restartButton.addEventListener("click", () => timeline.restart());
-//
-//     slider.addEventListener("input", () => {
-//         timeline.progress(slider.value).pause();
-//     });
-//
-//     timeline.eventCallback("onUpdate", () => {
-//         slider.value = timeline.progress();
-//     });
-// }
-// createTimelineControls(cloud12Tl, 'cloud12Tl');
+    window.addEventListener('scroll', checkVisibility);
+    window.addEventListener('resize', checkVisibility);
+    window.addEventListener('load', checkVisibility);
+})
